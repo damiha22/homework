471fd680533ab9223b83ec22b2cf6eb21c53e05c
damiha22 <109446791+damiha22@users.noreply.github.com>
2022-07-29 11:33:30 +0900

    Create printstar.cs

---
+++ ConsoleApp1/printstar.cs
@@ -0,0 +1,68 @@
+﻿using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class printstar
+    {
+        static void Main()
+        {
+            string[,] cells = {
+                {"*", " "," "," "," "},
+                {"*", "*"," "," "," "},
+                {"*", "*","*"," "," "},
+                {"*", "*","*","*"," "},
+                {"*", "*","*","*","*"}
+            };      
+            int i, j, k;
+
+            for (i = 0; i <=4; i++)
+            {
+                for (j = 0; j <=4; j++)
+                    Write(cells[i, j]);
+                Write("\n");
+            }
+
+            Write("\n");
+
+            for (i = 0; i <= 4; i++)
+            {
+                for (j = 4; j >= 0; j--)
+                    Write(cells[i, j]);
+                Write("\n");
+            }
+
+            Write("\n");
+
+            for (i = 0; i <= 4; i++)
+            {
+                for (j = 4; j >=0; j--)
+                    Write(cells[i, j]);
+                for(k=1; k<=4; k++)
+                    Write(cells[i, k]);
+                Write("\n");
+            }
+
+            Write("\n");
+            for (i = 0; i <= 4; i++)
+            {
+                for (j = 4; j >= 0; j--)
+                    Write(cells[i, j]);
+                for (k = 1; k <= 4; k++)
+                    Write(cells[i, k]);
+                Write("\n");
+            }
+            for (i = 3; i >= 0; i--)
+            {
+                for (j = 4; j >= 0; j--)
+                    Write(cells[i, j]);
+                for (k = 1; k <= 4; k++)
+                    Write(cells[i, k]);
+                Write("\n");
+            }
+        }
+    }
+}
