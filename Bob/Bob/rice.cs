69dde0ec98bb2672e75652de7ecf721d60aa729a
damiha22 <109446791+damiha22@users.noreply.github.com>
2022-07-30 19:46:30 +0900

    hw
    hw

---
+++ Bob/Bob/rice.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bob
+{
+    class Rice : Koreanfood
+    {
+        int carbohydrate;
+        int empty;
+
+        Rice()
+        {
+
+        }
+        public override int Eat(int carbohydrate)
+        {
+            return carbohydrate;
+        }
+
+        public override int Noteat(int empty)
+        {
+            return empty;
+        }
+    }
+}
